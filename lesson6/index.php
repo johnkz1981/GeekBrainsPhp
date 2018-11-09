e59863bd6d543a34c1426137c83e76c6b7f0ecc8
johnkz <johnkz@inbox.ru>
2018-11-09 17:50:47 +0300

    Lesson 6 task 4 finish

--- lesson6/index.php
+++ lesson6/index.php
@@ -9,9 +9,15 @@
 
 include('bd.php');
 
-$res = mysqli_query($link, "SELECT img_max FROM img WHERE id = '$id'");
-$row = mysqli_fetch_assoc($res);
+function console($val)
+{
+  echo '<script>console.log(' . json_encode($val) . ')</script>';
+}
 
+$query = mysqli_query($link, 'SELECT * FROM goods');
+$id = !empty($_GET['id']) && is_numeric($_GET['id']) ? (int)$_GET['id'] : 0;
+$productQuery = mysqli_query($link, "SELECT * FROM goods where id = $id");
+$productRes = mysqli_fetch_assoc($productQuery);
 ?>
 
 <!doctype html>
@@ -22,8 +28,62 @@ $row = mysqli_fetch_assoc($res);
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
+  <style>
+    .table {
+      display: grid;
+      grid-template-columns: repeat(4, 1fr);
+      width: 800px;
+    }
+
+    .detail {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      width: 400px;
+      margin-top: 50px;
+      border: solid 1px orangered;
+      padding: 5px;
+    }
+
+    .head {
+      color: blue;
+      font-size: 18px;
+      margin-bottom: 5px;
+    }
+
+    .detail__column {
+      display: flex;
+      flex-direction: column;
+    }
+  </style>
 </head>
 <body>
-
+<form action="">
+  <div class="table">
+    <label class="head">Title</label>
+    <label class="head">Description</label>
+    <label class="head">price</label>
+    <label class="head">action</label>
+    <? while ($row = mysqli_fetch_assoc($query)) : ?>
+      <div><?= $row['good'] ?></div>
+      <div><?= $row['description'] ?></div>
+      <div><?= $row['price'] ?></div>
+      <div>
+        <button value="<?= $row['id'] ?>" name="id">get</button>
+      </div>
+    <? endwhile ?>
+  </div>
+</form>
+<div class="detail" <? if (!$id): ?> style="display: none" <? endif ?>>
+  <div class="detail__column">
+    <label for="">Title</label>
+    <label for="">Description</label>
+    <label for="">Price</label>
+  </div>
+  <div class="detail__column">
+    <label for=""><?= $productRes['good'] ?></label>
+    <label for=""><?= $productRes['description'] ?></label>
+    <label for=""><?= $productRes['price'] ?></label>
+  </div>
+</div>
 </body>
 </html>
